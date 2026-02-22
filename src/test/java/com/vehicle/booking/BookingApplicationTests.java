bba584a3a761637164f41e045f563325ad16e964
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2026-02-22 12:07:02 +0530

    first commit

---
+++ src/test/java/com/vehicle/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicle.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
